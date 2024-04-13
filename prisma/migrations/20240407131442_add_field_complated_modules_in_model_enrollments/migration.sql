50ac0680ba303d9709b438b918700be60bd8b4cb
Asygarr <105841104721@student.unismuh.ac.id>
2024-04-13 14:14:09 +0800

    add field complatedModulues in model enrollments

---
+++ prisma/migrations/20240407131442_add_field_complated_modules_in_model_enrollments/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Enrollments" ADD COLUMN     "complatedModules" INTEGER[];
---
+++ prisma/migrations/20240407132136_add_default_for_field_complated_modules/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Enrollments" ALTER COLUMN "complatedModules" SET DEFAULT ARRAY[]::INTEGER[];
